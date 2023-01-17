d331cadaa764a72e57e6463e7d15f4a1df8fa3b2
Mikhail Yudin <fagci.nsk@gmail.com>
2023-01-18 02:48:56 +0700

    feat: palettes

--- canvas.js
+++ canvas.js
@@ -1,178 +1,172 @@
 export default class Canvas {
-  constructor() {
-    this.ctxBG = fftBG.getContext("2d");
-    this.ctxSpectrum = spectrum.getContext("2d", { alpha: false });
-    this.ctxFft = fft.getContext("2d");
-    this.resize()
-  }
-
-  i2Hz(i) {
-    return i * this.sampleRate / (this.data.length * 2)
-  }
-
-  start(audio) {
-    this.d = [];
-    this.data = audio.getFreqData();
-    this.sampleRate = audio.getSampleRate();
-    this.normalizeData = this.data.length > this.W ? this.averageData : this.interpolateData;
-    this.onUpdateCallback = audio.updateFreqData;
-    this.resize()
-    this.draw();
-  }
-
-  stop() {
-    cancelAnimationFrame(this.animationFrame);
-  }
-
-  resize() {
-    this.W = fft.width = fftBG.width = spectrum.width = fft.parentElement.clientWidth;
-    this.H = fftBG.height = fft.parentElement.clientHeight;
-
-    this.fftH = fft.height = fftBG.height * 0.2
-    this.spectrumH = spectrum.height = fftBG.height * 0.8 - 16
-
-    spectrum.style.top = this.fftH + 16 + "px"
-
-    if (this.data) {
-      this.scaleX = this.W / this.data.length;
-    }
-    this.fftScaleY = this.fftH / 256;
-    this.ctxFft.strokeStyle = '#aff';
-
-    this.createColorGradient()
-
-    if (this.data) this.drawBG();
-  }
-
-  draw() {
-    this.animationFrame = requestAnimationFrame(this.draw.bind(this));
-    this.onUpdateCallback();
-    this.normalizeData();
-    this.drawGraph();
-    this.drawSpectrum();
-  }
-
-  averageData() {
-    const data = this.data;
-    const dLen = data.length;
-    const W = this.W;
-    const scaleX = this.scaleX;
-    const d = this.d;
-
-    const c = [];
-    for (let x = 0; x < W; ++x) {
-      d[x] = 0;
-      c[x] = 0;
+    constructor() {
+        this.ctxBG = fftBG.getContext("2d");
+        this.ctxSpectrum = spectrum.getContext("2d", { alpha: false });
+        this.ctxFft = fft.getContext("2d");
+        this.resize()
     }
 
-    for (let i = 0; i < dLen; ++i) {
-      const xi = (i * scaleX) | 0;
-      d[xi] += data[i];
-      c[xi]++;
+    i2Hz(i) {
+        return i * this.sampleRate / (this.data.length * 2)
     }
 
-    for (let x = 0; x < W; ++x) {
-      d[x] = (d[x] / c[x]) | 0;
+    start(audio) {
+        this.d = [];
+        this.data = audio.getFreqData();
+        this.sampleRate = audio.getSampleRate();
+        this.normalizeData = this.data.length > this.W ? this.averageData : this.interpolateData;
+        this.onUpdateCallback = audio.updateFreqData;
+        this.resize()
+        this.draw();
     }
-  }
 
-  interpolateData() {
-    const data = this.data;
-    const W = this.W;
-    const d = this.d;
-    const scaleX = this.scaleX;
-    for (let x = 0; x < W; ++x) {
-      d[x] = data[(x / scaleX) | 0];
+    stop() {
+        cancelAnimationFrame(this.animationFrame);
     }
-  }
 
-  drawBG() {
-    const F_GRAD = 1000;
+    resize() {
+        this.W = fft.width = fftBG.width = spectrum.width = fft.parentElement.clientWidth;
+        this.H = fftBG.height = fft.parentElement.clientHeight;
 
-    const ctx = this.ctxBG;
-    let nextF = F_GRAD;
+        this.fftH = fft.height = fftBG.height * 0.2
+        this.spectrumH = spectrum.height = fftBG.height * 0.8 - 16
 
-    ctx.clearRect(0, 0, this.W, this.H);
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'top';
-    ctx.fillStyle = '#eee';
-    ctx.strokeStyle = '#444';
+        spectrum.style.top = this.fftH + 16 + "px"
 
-    ctx.lineWidth = 1;
+        if (this.data) {
+            this.scaleX = this.W / this.data.length;
+        }
+        this.fftScaleY = this.fftH / 256;
+        this.ctxFft.strokeStyle = '#aff';
 
-    ctx.beginPath();
-
-    ctx.moveTo(0, this.fftH);
-    ctx.lineTo(this.W, this.fftH);
-    ctx.stroke();
-
-    ctx.setLineDash([3, 5]);
-    ctx.beginPath();
-    for (let i = 0, x = 0; i < this.data.length, x < this.W; i++, x += this.scaleX) {
-      let cf = this.i2Hz(i);
-      if (cf >= nextF) {
-        ctx.moveTo((x | 0) - 0.5, 0);
-        ctx.lineTo((x | 0) - 0.5, this.fftH);
-        ctx.fillText(nextF / 1000 + "k", x, this.fftH + 4);
-        nextF += F_GRAD;
-      }
+        if (this.data) this.drawBG();
     }
-    ctx.stroke();
-  }
 
-  drawGraph() {
-    const ctx = this.ctxFft;
-    const fftH = this.fftH;
-    const data = this.d;
-    const fftScaleY = this.fftScaleY;
-    const W = this.W;
-
-    ctx.clearRect(0, 0, W, fftH);
-    ctx.beginPath();
-    ctx.moveTo(0, fftH);
-    for (let x = 0; x < W; ++x) {
-      ctx.lineTo(x + 0.5, ((fftH - data[x] * fftScaleY) | 0) + 0.5);
+    draw() {
+        this.animationFrame = requestAnimationFrame(this.draw.bind(this));
+        this.onUpdateCallback();
+        this.normalizeData();
+        this.drawGraph();
+        this.drawSpectrum();
     }
-    ctx.stroke();
-  }
 
-  drawSpectrum() {
-    const ctx = this.ctxSpectrum;
-    const spectrumH = this.spectrumH;
-    const data = this.d;
-    const colors = this.colors;
-    const W = this.W;
+    averageData() {
+        const data = this.data;
+        const dLen = data.length;
+        const W = this.W;
+        const scaleX = this.scaleX;
+        const d = this.d;
 
-    let imageData = ctx.getImageData(0, 0, W, spectrumH);
-    ctx.putImageData(imageData, 0, 1);
-    imageData = ctx.getImageData(0, spectrumH - 1, W, 1);
+        const c = [];
+        for (let x = 0; x < W; ++x) {
+            d[x] = 0;
+            c[x] = 0;
+        }
 
-    for (let x = 0; x < W; ++x) {
-      let ind = x * 4;
-      const p = colors[data[x]];
-      imageData.data[ind] = p[0];
-      imageData.data[ind + 1] = p[1];
-      imageData.data[ind + 2] = p[2];
+        for (let i = 0; i < dLen; ++i) {
+            const xi = (i * scaleX) | 0;
+            d[xi] += data[i];
+            c[xi]++;
+        }
+
+        for (let x = 0; x < W; ++x) {
+            d[x] = (d[x] / c[x]) | 0;
+        }
     }
-    ctx.putImageData(imageData, 0, 0);
-  }
 
-  createColorGradient() {
-    const gradient = this.ctxSpectrum.createLinearGradient(0, 0, 255, 0);
-    gradient.addColorStop(1, '#400');
-    gradient.addColorStop(0.62, '#f00');
-    gradient.addColorStop(0.54, '#f80');
-    gradient.addColorStop(0.47, '#ff0');
-    gradient.addColorStop(0.39, '#fff');
-    gradient.addColorStop(0.31, '#08f');
-    gradient.addColorStop(0.15, '#008');
-    gradient.addColorStop(0.05, '#004');
-    gradient.addColorStop(0, '#000');
-    this.ctxSpectrum.fillStyle = gradient;
-    this.ctxSpectrum.fillRect(0, 1, 256, 5);
-    this.colors = [];
-    for (let i = 0; i < 256; i++) {
-      this.colors[i] = this.ctxSpectrum.getImageData(i, 1, 1, 1).data;
+    interpolateData() {
+        const data = this.data;
+        const W = this.W;
+        const d = this.d;
+        const scaleX = this.scaleX;
+        for (let x = 0; x < W; ++x) {
+            d[x] = data[(x / scaleX) | 0];
+        }
     }
-  }
+
+    drawBG() {
+        const F_GRAD = 1000;
+
+        const ctx = this.ctxBG;
+        let nextF = F_GRAD;
+
+        ctx.clearRect(0, 0, this.W, this.H);
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'top';
+        ctx.fillStyle = '#eee';
+        ctx.strokeStyle = '#444';
+
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+
+        ctx.moveTo(0, this.fftH);
+        ctx.lineTo(this.W, this.fftH);
+        ctx.stroke();
+
+        ctx.setLineDash([3, 5]);
+        ctx.beginPath();
+        for (let i = 0, x = 0; i < this.data.length, x < this.W; i++, x += this.scaleX) {
+            let cf = this.i2Hz(i);
+            if (cf >= nextF) {
+                ctx.moveTo((x | 0) - 0.5, 0);
+                ctx.lineTo((x | 0) - 0.5, this.fftH);
+                ctx.fillText(nextF / 1000 + "k", x, this.fftH + 4);
+                nextF += F_GRAD;
+            }
+        }
+        ctx.stroke();
+    }
+
+    drawGraph() {
+        const ctx = this.ctxFft;
+        const fftH = this.fftH;
+        const data = this.d;
+        const fftScaleY = this.fftScaleY;
+        const W = this.W;
+
+        ctx.clearRect(0, 0, W, fftH);
+        ctx.beginPath();
+        ctx.moveTo(0, fftH);
+        for (let x = 0; x < W; ++x) {
+            ctx.lineTo(x + 0.5, ((fftH - data[x] * fftScaleY) | 0) + 0.5);
+        }
+        ctx.stroke();
+    }
+
+    drawSpectrum() {
+        const ctx = this.ctxSpectrum;
+        const spectrumH = this.spectrumH;
+        const data = this.d;
+        const colors = this.colors;
+        const W = this.W;
+
+        let imageData = ctx.getImageData(0, 0, W, spectrumH);
+        ctx.putImageData(imageData, 0, 1);
+        imageData = ctx.getImageData(0, spectrumH - 1, W, 1);
+
+        for (let x = 0; x < W; ++x) {
+            let ind = x * 4;
+            const p = colors[data[x]];
+            imageData.data[ind] = p[0];
+            imageData.data[ind + 1] = p[1];
+            imageData.data[ind + 2] = p[2];
+        }
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    setPalette(palette) {
+        const gradient = this.ctxSpectrum.createLinearGradient(0, 0, 255, 0);
+        const paletteSize = palette.length;
+        palette.forEach((c, i) => {
+            const cStop = i / paletteSize;
+            gradient.addColorStop(cStop, c);
+        });
+        this.ctxSpectrum.fillStyle = gradient;
+        this.ctxSpectrum.fillRect(0, 1, 256, 5);
+        this.colors = [];
+        for (let i = 0; i < 256; i++) {
+            this.colors[i] = this.ctxSpectrum.getImageData(i, 1, 1, 1).data;
+        }
+    }
 }
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import palettes from "./palettes.js"
 import Canvas from './canvas.js'
 import Audio from './audio.js'
 
@@ -6,60 +7,77 @@ let audio;
 let canvas;
 
 function updateGain() {
-  audio.setGain(this.value)
+    audio.setGain(this.value)
 }
 
 function updateFftSize() {
-  canvas.stop();
-  audio.setFftSize(this.value)
-  canvas.start(audio);
+    canvas.stop();
+    audio.setFftSize(this.value)
+    canvas.start(audio);
 }
 
 async function onDeviceSelect() {
-  canvas.stop();
+    canvas.stop();
 
-  if (!audio) {
-    audio = new Audio(fftSize.value, inputGain.value);
+    if (!audio) {
+        audio = new Audio(fftSize.value, inputGain.value);
 
-    inputGain.addEventListener('change', updateGain);
-    inputGain.addEventListener('mousemove', updateGain);
-    inputGain.addEventListener('touchmove', updateGain);
-    fftSize.addEventListener('change', updateFftSize);
-  }
+        inputGain.addEventListener('change', updateGain);
+        inputGain.addEventListener('mousemove', updateGain);
+        inputGain.addEventListener('touchmove', updateGain);
+        fftSize.addEventListener('change', updateFftSize);
+    }
 
-  try {
-    audio.start(src.value);
-    canvas.start(audio);
-  } catch (err) {
-    alert(`${err.message}\nTry to reload app and select same device`)
-    console.error(err)
-  }
+    try {
+        audio.start(src.value);
+        canvas.start(audio);
+    } catch (err) {
+        alert(`${err.message}\nTry to reload app and select same device`)
+        console.error(err)
+    }
 }
 
 async function getMediaDevices(type) {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  return devices.filter(device => device.kind === type)
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices.filter(device => device.kind === type)
 }
 
 function createOption(d, i) {
-  return `<option value="${d.deviceId}">${d.label ? d.label : 'Input ' + i}</option>`;
+    return `<option value="${d.deviceId}">${d.label ? d.label : 'Input ' + i}</option>`;
 }
 
 async function updateDevicesList() {
-  const devices = await getMediaDevices('audioinput');
-  src.innerHTML = "<option>Select device</option>" + devices.map(createOption).join("");
+    const devices = await getMediaDevices('audioinput');
+    src.innerHTML = "<option>Select device</option>" + devices.map(createOption).join("");
+}
+
+async function onPaletteSelect() {
+    const palette = palettes[palettesSelect.value];
+    canvas.setPalette(palette);
+}
+
+function initPalettesList() {
+    palettesSelect.innerHTML =
+        Object.keys(palettes)
+            .map(p => `<option value="${p}">${p}</option>`)
+            .join("");
+    palettesSelect.value = 'gqrx';
+    onPaletteSelect();
 }
 
 async function onPageLoad() {
-  canvas = new Canvas();
-  updateDevicesList()
-  navigator.mediaDevices.addEventListener('devicechange', updateDevicesList);
-  window.addEventListener("resize", canvas.resize.bind(canvas));
+    canvas = new Canvas();
+    initPalettesList();
+    updateDevicesList();
+    navigator.mediaDevices.addEventListener('devicechange', updateDevicesList);
+    window.addEventListener("resize", canvas.resize.bind(canvas));
+
 
     try {
         await navigator.wakeLock.request('screen');
-    } catch (_) {}
+    } catch (_) { }
 }
 
+palettesSelect.addEventListener("change", onPaletteSelect);
 src.addEventListener("change", onDeviceSelect);
 window.addEventListener("load", onPageLoad);
